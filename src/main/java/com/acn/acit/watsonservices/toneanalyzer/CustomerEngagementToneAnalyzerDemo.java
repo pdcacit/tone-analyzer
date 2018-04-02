5209b0c842c9ab9028a067e184bd7066a0af9d9f
Fatima Hiprenoridel Hizon <fhchizon@gmail.com>
2018-04-02 19:14:30 +0800

    First Commit

---
+++ src/main/java/com/acn/acit/watsonservices/toneanalyzer/CustomerEngagementToneAnalyzerDemo.java
@@ -0,0 +1,38 @@
+package com.acn.acit.watsonservices.toneanalyzer;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.ToneAnalyzer;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneChatOptions;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.Utterance;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.UtteranceAnalyses;
+
+
+public class CustomerEngagementToneAnalyzerDemo {
+	/*	
+	 * Please refer on Tone_Analyzer_HandsOn.ppt
+	 * Visit API Reference https://www.ibm.com/watson/developercloud/tone-analyzer/api/v3/
+	 * Copy Maven dependency in pom.xml,
+	 * Update the pom dependency: Right click on project > Maven > Update Project
+	 * On API Reference: Click on JAVA tab.
+	 * On API Reference: Click on Methods > Analyze customer engagement tone
+	 * Copy and paste the code inside public static void main(String[] args){}
+	 * Import necessary libraries
+	 * Copy and change the version, username, password based on your tone analyzer service credentials.
+	 * To run using in eclipse: Right click on the project in eclipse > Run As > Java Application
+	 * To run using command prompt: Go to the project directory and type the command > 'mvn exec:java'
+	 * To rebuild type 'mvn package' or 'mvn verify' before executing 'mvn exec:java' command
+	*/	
+	public static void main(String[] args) {
+		//Paste code here...		
+	}
+}
---
+++ src/main/java/com/acn/acit/watsonservices/toneanalyzer/GeneralToneAnalyzerDemo.java
@@ -0,0 +1,30 @@
+package com.acn.acit.watsonservices.toneanalyzer;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.ToneAnalyzer;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneAnalysis;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneInput;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneOptions;
+
+public class GeneralToneAnalyzerDemo {
+	/*	
+	 * Please refer on Tone_Analyzer_HandsOn.ppt
+	 * Visit API Reference https://www.ibm.com/watson/developercloud/tone-analyzer/api/v3/ 
+	 * On API Reference: Click on JAVA tab.
+	 * On API Reference: Click on Methods > Analyze general tone
+	 * Copy and paste the code inside public static void main(String[] args){}
+	 * Import necessary libraries
+	 * Copy and change the version, username, password based on your tone analyzer service credentials.
+	 * To run using in eclipse: Right click on the project in eclipse > Run As > Java Application
+	 * To run using command prompt: Go to the project directory and type the command > 'mvn exec:java'
+	 * To rebuild type 'mvn package' or 'mvn verify' before executing 'mvn exec:java' command
+	*/			
+	public static void main(String[] args) {
+		//Paste code here..
+	}
+}
